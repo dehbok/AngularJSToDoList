beaca7970bd6a926444d0866c1acbf7f5ab1cdec
Denys Shamrai <dehbok@gmail.com>
2017-09-22 10:52:30 +0300

    beta

--- js/controllers/todoController.js
+++ js/controllers/todoController.js
@@ -2,23 +2,24 @@ app.controller('todoController', function todoController($scope, $firebaseArray)
 
     var url = 'https://angularjstodolist003.firebaseio.com/';
     var fireRef = new Firebase(url);
-
-
     $scope.items = $firebaseArray(fireRef);
-
-    console.log($scope.items);
 
     $scope.addItem = function () {
         $scope.items.$add({
             title: $scope.newItemTitle,
             done: false
         });
-        $scope.formTodoText = '';
+        $scope.newItemTitle = '';
 
     };
 
     $scope.removeItem = function (item) {
         $scope.items.$remove(item);
     };
-    
+
+    $scope.checkItem = function (item) {
+        console.log(item);
+        $scope.items.$save(item);
+    };
+
 });
